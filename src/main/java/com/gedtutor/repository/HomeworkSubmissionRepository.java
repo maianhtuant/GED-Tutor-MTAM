0af770561c6f07859c5eba735f329d8f7d45c592
Tuan Mai <maianhtuant@gmail.com>
2026-04-20 16:44:06 -0700

    Add GitHub actions deployment workflow

--- src/main/java/com/gedtutor/repository/HomeworkSubmissionRepository.java
+++ src/main/java/com/gedtutor/repository/HomeworkSubmissionRepository.java
@@ -4,6 +4,8 @@ import com.gedtutor.model.Homework;
 import com.gedtutor.model.HomeworkSubmission;
 import com.gedtutor.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +13,7 @@ import java.util.Optional;
 public interface HomeworkSubmissionRepository extends JpaRepository<HomeworkSubmission, Long> {
     Optional<HomeworkSubmission> findByHomeworkAndStudent(Homework homework, User student);
     List<HomeworkSubmission> findByStudentOrderBySubmittedAtDesc(User student);
-    List<HomeworkSubmission> findByHomeworkOrderBySubmittedAtDesc(Homework homework);
+
+    @Query("SELECT s FROM HomeworkSubmission s JOIN FETCH s.student WHERE s.homework = :homework ORDER BY s.submittedAt DESC")
+    List<HomeworkSubmission> findByHomeworkOrderBySubmittedAtDesc(@Param("homework") Homework homework);
 }
